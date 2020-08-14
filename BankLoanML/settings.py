0c22673ec85c96ac9b512d97171cabb95ff1d8dc
sudarAw <sudarshanawasthi47@gmail.com>
2020-08-14 21:52:29 +0200

    static path updated

--- BankLoanML/settings.py
+++ BankLoanML/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'loanform/static')
+    os.path.join(BASE_DIR,'static')
 )
