d4b1adc64e2569cc3ea1969f2e7ba64dfe48f62e
sudarAw <sudarshanawasthi47@gmail.com>
2020-08-14 22:12:30 +0200

    root files properly updated

--- BankLoanML/settings.py
+++ BankLoanML/settings.py
@@ -122,9 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'loanform/static')
 
-STATIC_ROOT= (
-    os.path.join(BASE_DIR,'static_cdn', 'static_root')
-)
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'loanform/static'),
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
