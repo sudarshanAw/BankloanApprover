4359b9142ef45431aa9199985764749d2ce97870
sudarAw <sudarshanawasthi47@gmail.com>
2020-08-15 23:20:07 +0200

    more final

--- BankLoanML/settings.py
+++ BankLoanML/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'BankLoanML.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Test',
+        'USER' : 'postgres',
+        'PASSWORD' : 'emperor1995',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
     }
 }
 
